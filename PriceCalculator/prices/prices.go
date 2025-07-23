5c3c209e7cc9f2d294327fcf63c341dbf33a1fd5
prashant <pndhavale@gmail.com>
2025-07-23 21:16:48 +1000

    Pricing Calculator

---
+++ PriceCalculator/prices/prices.go
@@ -0,0 +1,58 @@
+package prices
+
+import (
+	"fmt"
+
+	"example.com/price-calculator/conversion"
+	"example.com/price-calculator/filemanager"
+)
+
+type TaxIncludedPriceJob struct {
+	IOManager         filemanager.FileManager
+	TaxRate           float64
+	InputPrices       []float64
+	TaxIncludedPrices map[string]string
+}
+
+func (job *TaxIncludedPriceJob) LoadData() {
+
+	lines, err := job.IOManager.ReadLines()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	prices, err := conversion.StringsToFloats(lines)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	job.InputPrices = prices
+}
+
+func (job TaxIncludedPriceJob) Process() {
+
+	job.LoadData()
+
+	result := make(map[string]string)
+	for _, price := range job.InputPrices {
+		taxIncludedPrice := price * (1 + job.TaxRate)
+		result[fmt.Sprintf("%.2f", price)] = fmt.Sprintf("%.2f", taxIncludedPrice)
+	}
+
+	job.TaxIncludedPrices = result
+	err := job.IOManager.WriteResults(job)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+}
+
+func NewTaxIncludedPriceJob(fm filemanager.FileManager, taxRate float64) *TaxIncludedPriceJob {
+	return &TaxIncludedPriceJob{
+		InputPrices: []float64{10, 20, 30},
+		TaxRate:     taxRate,
+		IOManager:   fm,
+	}
+}
